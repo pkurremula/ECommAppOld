c8bf0534e6f173ade77f90aa479411dee0754fe5
pkurremula <pkurremula@gmail.com>
2022-05-08 03:34:48 -0400

    Initial Commit

---
+++ ECommAppInfra/Data/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using ECommAppCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommAppInfra.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products
+        {
+            get;
+            set;
+        }
+    }
+}
